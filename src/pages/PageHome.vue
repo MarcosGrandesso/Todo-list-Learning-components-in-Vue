efd3c443c9b997cb8c75a54ad0c3dac4437730f5
Marcos Grandesso <marcosgrandesso@hotmail.com>
2022-09-08 14:56:55 -0300

    segunda versao

---
+++ src/pages/PageHome.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <TarefaList msg="Welcome to Your Vue.js App" :tasks="listaDeTarefa" />
+    <p>{{ listaDeTarefa }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import TarefaList from "../components/TarefaList.vue";
+
+export default {
+  components: {
+    TarefaList,
+  },
+  data: () => {
+    return { listaDeTarefa: ["a", "b", "c"] };
+  },
+  created() {
+    console.log("terminei de carreagr a pag");
+    axios.get("http://localhost:3000/tasks/").then((response) => {
+      console.log(response.data);
+      this.listaDeTarefa = response.data;
+    });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
